3237a6ae75c8665fdf07a4edbf431235eae3eeb8
Tomasz Nurkiewicz <tomasz.nurkiewicz@allegrogroup.com>
2017-10-02 10:01:05 -0700

    Upgrading Antlr 4.5.3 -> 4.7

--- src/main/java/com/nurkiewicz/tsclass/parser/Parser.java
+++ src/main/java/com/nurkiewicz/tsclass/parser/Parser.java
@@ -4,27 +4,27 @@ import com.nurkiewicz.tsclass.antlr.parser.TypeScriptLexer;
 import com.nurkiewicz.tsclass.antlr.parser.TypeScriptParser;
 import com.nurkiewicz.tsclass.parser.ast.ClassDescriptor;
 import com.nurkiewicz.tsclass.parser.visitors.ClassVisitor;
-import org.antlr.v4.runtime.ANTLRInputStream;
 import org.antlr.v4.runtime.BaseErrorListener;
 import org.antlr.v4.runtime.CharStream;
+import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.RecognitionException;
 import org.antlr.v4.runtime.Recognizer;
 import org.antlr.v4.runtime.TokenStream;
-import org.apache.commons.io.IOUtils;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
 
 public class Parser {
 
-    public ClassDescriptor parse(InputStream sourceCode) throws IOException {
-        return parse(IOUtils.toString(sourceCode, StandardCharsets.UTF_8));
+    public ClassDescriptor parse(String sourceCode) throws IOException {
+        return parse(new ByteArrayInputStream(sourceCode.getBytes(StandardCharsets.UTF_8)));
     }
 
-    public ClassDescriptor parse(String sourceCode) throws IOException {
-        final CharStream charStream = new ANTLRInputStream(sourceCode);
+    public ClassDescriptor parse(InputStream sourceCode) throws IOException {
+        final CharStream charStream = CharStreams.fromStream(sourceCode);
         final TypeScriptLexer lexer = new TypeScriptLexer(charStream);
         final TokenStream tokens = new CommonTokenStream(lexer);
         final TypeScriptParser parser = new TypeScriptParser(tokens);
