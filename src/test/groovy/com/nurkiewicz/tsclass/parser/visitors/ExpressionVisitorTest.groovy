c181146827570ad6f8a4011f8bd1455191855b99
Tomasz Nurkiewicz <tomasz.nurkiewicz@allegrogroup.com>
2017-10-03 17:35:08 -0700

    Refactored tests

--- src/test/groovy/com/nurkiewicz/tsclass/parser/visitors/ExpressionVisitorTest.groovy
+++ src/test/groovy/com/nurkiewicz/tsclass/parser/visitors/ExpressionVisitorTest.groovy
@@ -15,6 +15,7 @@ import spock.lang.Unroll
 import static com.nurkiewicz.tsclass.parser.ast.expr.AdditiveExpression.Operator.MINUS
 import static com.nurkiewicz.tsclass.parser.ast.expr.AdditiveExpression.Operator.PLUS
 import static com.nurkiewicz.tsclass.parser.ast.expr.AdditiveExpression.add
+import static com.nurkiewicz.tsclass.parser.ast.expr.AdditiveExpression.sub
 import static com.nurkiewicz.tsclass.parser.ast.expr.Identifier.ident
 import static com.nurkiewicz.tsclass.parser.ast.expr.MultiplicativeExpression.Operator.DIV
 import static com.nurkiewicz.tsclass.parser.ast.expr.MultiplicativeExpression.Operator.MOD
@@ -47,41 +48,6 @@ class ExpressionVisitorTest extends Specification {
             '2 - 1.5' || 2 | 1.5 | MINUS
     }
 
-    def 'should parse complex addition expression'() {
-        when:
-            AdditiveExpression additive = parse('1 + 2 - 3 + 4 + 5')
-        then:
-            additive == new AdditiveExpression(
-                    new AdditiveExpression(
-                            new AdditiveExpression(
-                                    new AdditiveExpression(num(1), PLUS, num(2)),
-                                    MINUS,
-                                    num(3)
-                            ),
-                            PLUS,
-                            num(4)
-                    ),
-                    PLUS,
-                    num(5)
-            )
-    }
-
-    def 'should parse complex multiplication expression'() {
-        when:
-            MultiplicativeExpression multiplicative = parse('1 * 2 / 3 * 4 % 5')
-        then:
-            multiplicative == mod(
-                    mul(
-                            div(
-                                    mul(num(1), num(2)),
-                                    num(3)
-                            ),
-                            num(4)
-                    ),
-                    num(5)
-            )
-    }
-
     def 'should parse multiplication #expr'() {
         when:
             MultiplicativeExpression multiplicative = parse(expr)
@@ -96,26 +62,19 @@ class ExpressionVisitorTest extends Specification {
             '9 % 4'   || 9 | 4   | MOD
     }
 
-    def 'should take multiplication over addition precedence'() {
-        when:
-            AdditiveExpression addition = parse('2 + 3 * x')
-        then:
-            addition == new AdditiveExpression(
-                    num(2),
-                    PLUS,
-                    new MultiplicativeExpression(num(3), MUL, ident("x"))
-            )
-    }
-
     def 'should parse #expr to #obj'() {
         expect:
             parse(expr) == obj
         where:
             expr                || obj
             '2 + 3 * 6'         || add(num(2), mul(num(3), num(6)))
+            '2 + 3 * x'         || add(num(2), mul(num(3), ident("x")))
             '(2 + 3) * x'       || mul(add(num(2), num(3)), ident("x"))
             '2 + 3 / 4 + 5 * 6' || add(add(num(2), div(num(3), num(4))), mul(num(5), num(6)))
+            '1 + 2 - 3 + 4 + 5' || add(add(sub(add(num(1), num(2)), num(3)), num(4)), num(5))
+            '1 * 2 / 3 * 4 % 5' || mod(mul(div(mul(num(1), num(2)), num(3)), num(4)), num(5))
             '(x * y) + (a / b)' || add(mul(ident("x"), ident("y")), div(ident("a"), ident("b")))
+
     }
 
     private static Expression parse(String value) {
