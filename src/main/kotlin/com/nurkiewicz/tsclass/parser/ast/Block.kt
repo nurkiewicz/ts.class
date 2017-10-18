2f6ed852152182e66cbfeb8ea6615f739153f74a
Tomasz Nurkiewicz <tomasz.nurkiewicz@allegrogroup.com>
2017-10-18 23:30:34 +0200

    #5 | Parsing single-statement if-else construct

--- src/main/kotlin/com/nurkiewicz/tsclass/parser/ast/Block.kt
+++ src/main/kotlin/com/nurkiewicz/tsclass/parser/ast/Block.kt
@@ -1,4 +1,15 @@
 package com.nurkiewicz.tsclass.parser.ast
 
 
-data class Block(val statements: List<Statement>)
+data class Block(val statements: List<Statement>) {
+
+    override fun toString() = statements.toString()
+
+    companion object {
+
+        @JvmStatic
+        fun block(statements: List<Statement>) = Block(statements)
+
+    }
+
+}
